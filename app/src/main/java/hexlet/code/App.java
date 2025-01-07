3d55acb1ba45c4a17b9c73ce0488e710fb50b691
kiriert <lohovic6@gmail.com>
2025-01-07 23:31:25 +0400

    Error correction

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,7 +2,12 @@ package hexlet.code;
 
 import java.util.Scanner;
 
-import hexlet.code.games.*;
+import hexlet.code.games.Cli;
+import hexlet.code.games.GCD;
+import hexlet.code.games.Even;
+import hexlet.code.games.Calc;
+import hexlet.code.games.Progression;
+import hexlet.code.games.Prime;
 
 class App {
     public static void main(String[] args) {
